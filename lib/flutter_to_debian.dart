1099a9b6d0e411a159789bc4d056393c7fbc98b5
Rich Young <ry@richyoung.ca>
2024-09-30 22:55:22 -0400

    attempt to override arch in control file

--- lib/flutter_to_debian.dart
+++ lib/flutter_to_debian.dart
@@ -98,10 +98,20 @@ class FlutterToDebian {
     }
     if (yamlMap.containsKey('control')) {
       final control = yamlMap["control"];
+
+      var archOverride = "";
+
+      if (flutterArch != debianControl.debArch) {
+        archOverride = flutterArch;
+      }
+      else {
+        archOverride = control["Architecture"];
+      }
+
       debianControl = debianControl.copyWith(
         version: control["Version"],
         package: control["Package"],
-        debArch: control["Architecture"],
+        debArch: archOverride,
         maintainer: control["Maintainer"],
         description: control["Description"],
       );
